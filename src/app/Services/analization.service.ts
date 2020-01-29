4c8932522ec4f078601e73ae8985460e9c89ad6a
Мирослав <krystallsilver@hotmail.com>
2020-01-30 01:15:54 +0200

    changes

--- src/app/Services/analization.service.ts
+++ src/app/Services/analization.service.ts
@@ -2,30 +2,34 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Alternative } from '../Models/alternative.model';
 
+const corsUrl = "https://cors-anywhere.herokuapp.com/";
+
 @Injectable({
   providedIn: 'root'
 })
 export class AnalizatorService {
 
     urlApi = "https://vnforqa-api.herokuapp.com/api/Analizator/";
+    corsUrlApi = corsUrl + this.urlApi;
+
     constructor(private http:HttpClient) { }    
  
     GetSaatyTable(array: Alternative[]) {
-      return this.http.post(this.urlApi+"Saaty",array);
+      return this.http.post(this.corsUrlApi+"Saaty",array);
     }  
 
     GetMarkSaatyTable(markArray: Alternative[]) {
-      return this.http.post(this.urlApi+"UnrangetSaaty",markArray);
+      return this.http.post(this.corsUrlApi+"UnrangetSaaty",markArray);
     }
 
     GetKolSaatyTable(array: Alternative[]) {
-      return this.http.post(this.urlApi+"KolSaaty",array);
+      return this.http.post(this.corsUrlApi+"KolSaaty",array);
     }  
 
     GetSaaty99Table(array: Alternative[]) {
-      return this.http.post(this.urlApi+"Saaty99",array);
+      return this.http.post(this.corsUrlApi+"Saaty99",array);
     }  
     GetVisualSaaty(array: Alternative[]) {
-      return this.http.post(this.urlApi+"Geometry",array);
+      return this.http.post(this.corsUrlApi+"Geometry",array);
     }  
   }
--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Alternative } from "../Models/alternative.model";
 
+const corsUrl = "https://cors-anywhere.herokuapp.com/";
 
 @Injectable({
     providedIn: 'root'
@@ -12,24 +13,25 @@ export class UserService {
     constructor(private http: HttpClient) {}
 
     urlApi = "https://vnforqa-api.herokuapp.com/api/SampleData/";
+    corsUrlApi = corsUrl + this.urlApi;
 
     newUser() {
-        return this.http.post(this.urlApi+"User",'');
+        return this.http.post(this.corsUrlApi+"User",'');
     }
 
     setCookies(alternatives: Alternative[], type: number) {
-        return this.http.post(this.urlApi+"SetCookies",alternatives, { params: new HttpParams().set("type",type.toString())});
+        return this.http.post(this.corsUrlApi+"SetCookies",alternatives, { params: new HttpParams().set("type",type.toString())});
     }
     
     getCookies() {
-        return this.http.get(this.urlApi+"GetCookies");
+        return this.http.get(this.corsUrlApi+"GetCookies");
     }
 
     delete() {
-        return this.http.get(this.urlApi+"DelCookies");
+        return this.http.get(this.corsUrlApi+"DelCookies");
     }
 
     setUrel() {
-        return this.http.get(this.urlApi+"UserReload");
+        return this.http.get(this.corsUrlApi+"UserReload");
     }
 }
